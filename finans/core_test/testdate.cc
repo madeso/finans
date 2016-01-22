3b7cec047d2116a827222aac54fe2f25b964054d
Gustav <sir.gustav.the.coder@gmail.com>
2016-01-22 23:17:38 +0100

    test change members

--- finans/core_test/testdate.cc
+++ finans/core_test/testdate.cc
@@ -41,3 +41,19 @@ GTEST(TestConstructorLocal) {
   EXPECT_EQ(2, dt.day_of_moth());
   EXPECT_EQ(12, dt.hour());
 }
+
+GTEST(TestChangeMembers) {
+  auto dt = DateTime::FromDate(2010, Month::FEBRUARY, 2, TimeZone::LOCAL);
+  dt.set_year(2012);
+  dt.set_month(Month::JUNE);
+  dt.set_day_of_moth(10);
+  dt.set_hour(8);
+  dt.set_minutes(33);
+  dt.set_seconds(42);
+  EXPECT_EQ(2012, dt.year());
+  EXPECT_EQ(Month::JUNE, dt.month());
+  EXPECT_EQ(10, dt.day_of_moth());
+  EXPECT_EQ(8, dt.hour());
+  EXPECT_EQ(33, dt.minutes());
+  EXPECT_EQ(42, dt.seconds());
+}
