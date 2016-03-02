42ca1d70ecb9bb158432fe4519166c55a920c153
Gustav <sir.gustav.the.coder@gmail.com>
2016-03-02 23:06:19 +0100

    updated finans main to commandlineparser

--- finans/cmd/main.cc
+++ finans/cmd/main.cc
@@ -2,7 +2,7 @@
 
 #include "finans/core/finans.h"
 
-#include "tclap/CmdLine.h"
+#include "finans/core/commandline.h"
 
 const std::string USAGE_AND_HELP =
 "Usage: fin CMD ARGUMENTS\n"
@@ -16,11 +16,6 @@ const std::string USAGE_AND_HELP =
 int ExceptionHandler() {
   try {
     throw;
-  }
-  catch (TCLAP::ArgException &e)
-  {
-    std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
-    return -12;
   }
   catch (const std::string& error) {
     std::cerr << "error: " << error << "\n";
@@ -38,131 +33,159 @@ int ExceptionHandler() {
 
 //////////////////////////////////////////////////////////////////////////
 
-int cmd_status(std::vector<std::string> args) {
-  try {
-    TCLAP::CmdLine cmd("Give the status of your finances", ' ', "", false);
-    cmd.parse(args);
-
-    auto finans = Finans::CreateNew();
-    std::cout << "Number of accounts: " << finans->NumberOfAccounts() << "\n";
-    std::cout << "Number of companies: " << finans->NumberOfCompanies() << "\n";
-    std::cout << "Number of currencies: " << finans->NumberOfCurrencies() << "\n";
-    std::cout << "Number of categories: " << finans->NumberOfCategories() << "\n";
+class cmd_status : public argparse::SubParser {
+  void AddParser(argparse::Parser& parser) override {
   }
-  catch (...)
-  {
-    return ExceptionHandler();
+  void ParseCompleted() override {
+    try {
+      auto finans = Finans::CreateNew();
+      std::cout << "Number of accounts: " << finans->NumberOfAccounts() << "\n";
+      std::cout << "Number of companies: " << finans->NumberOfCompanies() << "\n";
+      std::cout << "Number of currencies: " << finans->NumberOfCurrencies() << "\n";
+      std::cout << "Number of categories: " << finans->NumberOfCategories() << "\n";
+    }
+    catch (...)
+    {
+      ExceptionHandler();
+    }
   }
+};
 
-  return 0;
-}
+class cmd_addcurrecy : public argparse::SubParser {
+  
+  std::string longNamneArg;
+  std::string shortNameArg;
+  std::string beforeArg;
+  std::string afterArg;
 
-int cmd_addcurrecy(std::vector<std::string> args) {
-  try {
-    TCLAP::CmdLine cmd("Add a currency to finans", ' ', "", false);
-    TCLAP::ValueArg<std::string> longNamneArg("n", "name", "The long name, ie. 'American Dollar'", true, "", "string", cmd);
-    TCLAP::ValueArg<std::string> shortNameArg("s", "short-name", "The short name ie. USD", true, "", "string", cmd);
-    TCLAP::ValueArg<std::string> beforeArg("b", "before", "The string before a value, ie. the $ in $99", false, "", "string", cmd);
-    TCLAP::ValueArg<std::string> afterArg("a", "after", "The string after a value, ie. the kr in 45 kr", false, "", "string", cmd);
-    cmd.parse(args);
-
-    auto finans = Finans::CreateNew();
-    finans->AddCurency(longNamneArg.getValue(), shortNameArg.getValue(), beforeArg.getValue(), afterArg.getValue());
-    finans->Save();
-    std::cout << "Added " << shortNameArg.getValue() << ".\n";
-  }
-  catch (...)
-  {
-    return ExceptionHandler();
+  void AddParser(argparse::Parser& parser) override {
+    parser.AddOption("name",       longNamneArg); //"The long name, ie. 'American Dollar'"
+    parser.AddOption("short-name", shortNameArg); //"The short name ie. USD"
+    parser.AddOption("before",     beforeArg   ); //"The string before a value, ie. the $ in $99"
+    parser.AddOption("after",      afterArg    ); //"The string after a value, ie. the kr in 45 kr"
   }
 
-  return 0;
-}
-
-int cmd_addaccount(std::vector<std::string> args) {
-  try {
-    TCLAP::CmdLine cmd("Add a account to finans", ' ', "", false);
-    TCLAP::ValueArg<std::string> longNamneArg("n", "name", "The name, ie. 'My card'", true, "", "string", cmd);
-    TCLAP::ValueArg<std::string> shortNameArg("s", "short-name", "The short name ie. Visa", true, "", "string", cmd);
-    TCLAP::ValueArg<std::string> currencyArg("c", "currency", "The default currency for this account", true, "", "string", cmd);
-    cmd.parse(args);
-
-    auto finans = Finans::CreateNew();
-    auto currency = finans->GetCurrencyByName(currencyArg.getValue());
-    if (currency == -1) throw "Unknown currency";
-    finans->AddAccount(longNamneArg.getValue(), shortNameArg.getValue(), currency);
-    finans->Save();
-    std::cout << "Added " << shortNameArg.getValue() << ".\n";
+  void ParseCompleted() override {
+    try {
+      auto finans = Finans::CreateNew();
+      finans->AddCurency(longNamneArg, shortNameArg, beforeArg, afterArg);
+      finans->Save();
+      std::cout << "Added " << shortNameArg << ".\n";
+    }
+    catch (...)
+    {
+      ExceptionHandler();
+    }
   }
-  catch (...)
-  {
-    return ExceptionHandler();
-  }
+};
 
-  return 0;
-}
+class cmd_addaccount : public argparse::SubParser {
+  std::string longNamneArg;
+  std::string shortNameArg;
+  std::string currencyArg;
 
-int cmd_addcompany(std::vector<std::string> args) {
-  try {
-    TCLAP::CmdLine cmd("Add a company to finans", ' ', "", false);
-    TCLAP::ValueArg<std::string> nameArg("n", "name", "The name, ie. 'Acme'", true, "", "string", cmd);
-    TCLAP::ValueArg<std::string> currencyArg("c", "currency", "The default currency this company works in", true, "", "string", cmd);
-    cmd.parse(args);
-
-    auto finans = Finans::CreateNew();
-    auto currency = finans->GetCurrencyByName(currencyArg.getValue());
-    if (currency == -1) throw "Unknown currency";
-    finans->AddCompany(nameArg.getValue(), currency);
-    finans->Save();
-    std::cout << "Added " << nameArg.getValue() << ".\n";
-  }
-  catch (...)
-  {
-    return ExceptionHandler();
+  void AddParser(argparse::Parser& parser) override {
+    // "Add a account to finans"
+    parser.AddOption("name",      longNamneArg); // "The name, ie. 'My card'"
+    parser.AddOption("short-name",shortNameArg); // "The short name ie. Visa"
+    parser.AddOption("currency",  currencyArg ); // "The default currency for this account"
   }
 
-  return 0;
-}
+  void ParseCompleted() override {
+    try {
 
-int cmd_addcategory(std::vector<std::string> args) {
-  try {
-    TCLAP::CmdLine cmd("Add a category/envelope to finans", ' ', "", false);
-    TCLAP::ValueArg<std::string> nameArg("n", "name", "The name, ie. 'Savings'", true, "", "string", cmd);
-    cmd.parse(args);
-
-    auto finans = Finans::CreateNew();
-    finans->AddCategory(nameArg.getValue());
-    finans->Save();
-    std::cout << "Added " << nameArg.getValue() << ".\n";
+      auto finans = Finans::CreateNew();
+      auto currency = finans->GetCurrencyByName(currencyArg);
+      if (currency == -1) throw "Unknown currency";
+      finans->AddAccount(longNamneArg, shortNameArg, currency);
+      finans->Save();
+      std::cout << "Added " << shortNameArg << ".\n";
+    }
+    catch (...)
+    {
+      ExceptionHandler();
+    }
   }
-  catch (...)
-  {
-    return ExceptionHandler();
-  }
+};
 
-  return 0;
-}
+class cmd_addcompany : public argparse::SubParser {
+  std::string nameArg;
+  std::string currencyArg;
 
-int cmd_install(std::vector<std::string> args) {
-  try {
-    TCLAP::CmdLine cmd("Install finans to your system", ' ', "", false);
-    TCLAP::ValueArg<std::string> folderArg("f", "folder", "Where to install", true, "", "string", cmd);
-    TCLAP::SwitchArg dontCreateSwitch("c", "dont-create", "Don't create the finans database", cmd, false);
-    cmd.parse(args);
-
-    const std::string folder = folderArg.getValue();
-    const bool dontCreate = dontCreateSwitch.getValue();
-
-    Finans::Install(folder, !dontCreate);
-    std::cout << "Install complete.\n";
-  }
-  catch (...)
-  {
-    return ExceptionHandler();
+  void AddParser(argparse::Parser& parser) override {
+     // "Add a company to finans"
+    parser.AddOption("name", nameArg); // "The name, ie. 'Acme'"
+    parser.AddOption("currency", currencyArg); // "The default currency this company works in"
   }
 
-  return 0;
-}
+  void ParseCompleted() override {
+    try {
+
+      auto finans = Finans::CreateNew();
+      auto currency = finans->GetCurrencyByName(currencyArg);
+      if (currency == -1) throw "Unknown currency";
+      finans->AddCompany(nameArg, currency);
+      finans->Save();
+      std::cout << "Added " << nameArg << ".\n";
+    }
+    catch (...)
+    {
+      ExceptionHandler();
+    }
+  }
+};
+
+class cmd_addcategory : public argparse::SubParser {
+  std::string nameArg;
+
+  void AddParser(argparse::Parser& parser) override {
+    // "Add a category/envelope to finans"
+    parser.AddOption("name", nameArg); // "The name, ie. 'Savings'"
+  }
+
+  void ParseCompleted() override {
+    try {
+
+      auto finans = Finans::CreateNew();
+      finans->AddCategory(nameArg);
+      finans->Save();
+      std::cout << "Added " << nameArg << ".\n";
+    }
+    catch (...)
+    {
+      ExceptionHandler();
+    }
+  }
+};
+
+class cmd_install : public argparse::SubParser {
+  std::string folderArg;
+  bool dontCreateSwitch;
+
+public:
+  cmd_install() : dontCreateSwitch(true) { }
+
+  void AddParser(argparse::Parser& parser) override {
+    // "Install finans to your system"
+    parser.AddOption("folder", folderArg); // "Where to install"
+    parser.StoreConst("dont-create", dontCreateSwitch, false);
+  }
+
+  void ParseCompleted() override {
+    try {
+
+      const std::string folder = folderArg;
+      const bool dontCreate = dontCreateSwitch;
+
+      Finans::Install(folder, !dontCreate);
+      std::cout << "Install complete.\n";
+    }
+    catch (...)
+    {
+      ExceptionHandler();
+    }
+  }
+};
 
 //////////////////////////////////////////////////////////////////////////
 
@@ -175,43 +198,23 @@ std::vector<std::string> Collect(int argc, char** argv) {
 }
 
 int main(int argc, char** argv) {
-  const auto args = Collect(argc, argv);
+  argparse::Parser parser("Finans command line client");
 
-  if (args.size() <= 1) {
-    std::cerr << "Missing command.\n";
-    std::cout << USAGE_AND_HELP;
-    return -1;
-  }
+  
+  cmd_status status;
+  parser.AddSubParser("status", &status);
+  cmd_install install;
+  parser.AddSubParser("install", &install);
+  cmd_addcurrecy addcurr;
+  parser.AddSubParser("addcurrency", &addcurr);
+  cmd_addaccount addacc;
+  parser.AddSubParser("addaccount", &addacc);
+  cmd_addcompany addcom;
+  parser.AddSubParser("addcompany", &addcom);
+  cmd_addcategory addcat;
+  parser.AddSubParser("addcategory", &addcat);
 
-  const auto cmd = args[1];
-
-  auto cmd_args = args;
-  cmd_args.erase(++cmd_args.begin()); // remove the second element
-  cmd_args[0] += " " + cmd;
-
-  if (cmd == "help") {
-    return 0;
-  }
-  if (cmd == "status" || cmd == "stat") {
-    return cmd_status(cmd_args);
-  }
-  if (cmd == "install") {
-    return cmd_install(cmd_args);
-  }
-  if (cmd == "addcurr") {
-    return cmd_addcurrecy(cmd_args);
-  }
-  if (cmd == "addacc") {
-    return cmd_addaccount(cmd_args);
-  }
-  if (cmd == "addcom") {
-    return cmd_addcompany(cmd_args);
-  }
-  if (cmd == "addcat") {
-    return cmd_addcategory(cmd_args);
-  }
-
-  std::cerr << "Unknown command " << cmd << ".\n";
-  std::cout << USAGE_AND_HELP;
-  return -2;
+  auto ret = parser.ParseArgs(argparse::Arguments(argc, argv));
+  if (ret == argparse::Parser::ParseFailed) return -1;
+  else return 0;
 }
